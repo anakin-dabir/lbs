505ab72ae1e486b7373087e8e428fa662a96cbbe
anakin-dabir <anakindabir@gmail.com>
2023-11-17 04:15:30 +0500

    Assing: manifest.json

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,35 @@ import * as tt from '@tomtom-international/web-sdk-maps';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 
 function App() {
+  const [userLocation, setUserLocation] = useState({lon: '', lat: ''});
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setUserLocation({lat: latitude, lon: longitude});
+        },
+        error => {
+          console.error(error.message);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported');
+    }
+  }, []);
   const [map, setMap] = useState();
   const mapContainer = useRef();
-  const AMSTERDAM = {
-    lon: 4.896029,
-    lat: 52.371807,
-  };
   useEffect(() => {
     let map = tt.map({
       key: 'nLGbvV7o3kUqMaiDZ7YHDMwMecBmH3M6',
       container: mapContainer.current.id,
-      center: AMSTERDAM,
+      center: userLocation,
       zoom: 10,
       language: 'en-GB',
     });
     map.addControl(new tt.FullscreenControl());
-
     map.addControl(new tt.NavigationControl());
-
     setMap(map);
-
     return () => {
       map.remove();
     };
