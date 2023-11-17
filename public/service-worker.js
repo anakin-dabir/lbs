39e2490e22de38777ac21c492ca5f1d41d405ec0
anakin-dabir <anakindabir@gmail.com>
2023-11-17 22:04:03 +0500

    modified:   public/service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,131 +1,17 @@
-const version = 1,
-  name = `my-app-v${version}`,
-  timeout = 1800,
-  urls = [],
-  hosts = [],
-  reload = false,
-  safari = true,
-  announce = true,
-  cacheable = arg => (arg.includes('no-store') || arg.includes('max-age=0')) === false;
-
-function log(arg) {
-  console.log(`[serviceWorker:${new Date().getTime()}] ${arg}`);
-}
-
-if (safari || /Version\/[\d+\.]+ Safari/.test(navigator.userAgent) === false) {
-  self.addEventListener('activate', ev =>
-    ev.waitUntil(
-      caches
-        .keys()
-        .then(args => {
-          const invalid = args.filter(i => i !== name);
-          let result;
-
-          if (args.includes(name) === false) {
-            caches
-              .open(name)
-              .then(cache => {
-                log('type=activate, cached=false, message="Caching core assets"');
-
-                return cache.addAll(urls);
-              })
-              .catch(err => log(`type=error, action=activate, message="${err.message}"`));
-          } else {
-            log('type=activate, cached=true, message="Reusing cached core assets"');
-          }
-
-          if (announce) {
-            self.clients.claim();
-            self.clients
-              .matchAll()
-              .then(clients => clients.forEach(client => client.postMessage(`version_${version}`)));
-          }
-
-          if (invalid.length === 0) {
-            log('type=delete, message="No stale caches"');
-            result = Promise.resolve();
-          } else {
-            log(`type=delete, message="Stale caches: ${invalid.toString()}"`);
-            result = Promise.all(
-              invalid.map(i => {
-                log(`type=delete, message="Deleted stale cache ${i}"`);
-                caches.delete(i);
-
-                if (reload) {
-                  self.clients.claim();
-                  self.clients.matchAll().then(clients =>
-                    clients.forEach(client => {
-                      log('type=reload, message="Loading new version of application"');
-                      client.postMessage('reload');
-                    })
-                  );
-                }
-              })
-            );
-          }
-
-          return result;
-        })
-        .catch(() => void 0)
-    )
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      console.log('cached');
+      return cache.addAll(['/']);
+    })
   );
+});
 
-  self.addEventListener('install', ev => {
-    self.skipWaiting();
-    ev.waitUntil(() => log('type=install, message="New service worker installed"'));
-  });
-
-  self.addEventListener('fetch', ev => {
-    const method = ev.request.method,
-      http = /^https?\:/.test(ev.request.url),
-      handle = hosts.length === 0 || hosts.includes(new URL(ev.request.url).hostname);
-    let result;
-
-    if (http && handle && method === 'GET') {
-      result = ev.respondWith(
-        caches
-          .open(name)
-          .then(cache => {
-            return cache.match(ev.request).then(cached => {
-              const now = new Date().getTime();
-              let lresult;
-
-              if (cached !== void 0) {
-                const url = new URL(cached.url),
-                  cdate = cached.headers.get('date'),
-                  then =
-                    (cdate !== null ? new Date(cdate) : new Date()).getTime() +
-                    Number(
-                      (cached.headers.get('cache-control') || '').replace(/[^\d]/g, '') || timeout
-                    ) *
-                      1e3;
-
-                if (urls.includes(url.pathname) || then > now) {
-                  lresult = cached.clone();
-                }
-              }
-
-              if (lresult === void 0) {
-                lresult = fetch(ev.request).then(res => {
-                  if (
-                    (res.type === 'basic' || res.type === 'cors') &&
-                    res.status === 200 &&
-                    cacheable(res.headers.get('cache-control') || '')
-                  ) {
-                    cache.put(ev.request, res.clone());
-                  }
-
-                  return res;
-                });
-              }
-
-              return lresult;
-            });
-          })
-          .catch(() => void 0)
-      );
-    }
-
-    return result;
-  });
-}
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      console.log('cach fetched');
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,8 +1,146 @@
-import React from 'react';
-import HomePage from './pages/HomePage';
+import axios from 'axios';
+import * as tt from '@tomtom-international/web-sdk-maps';
+import '@tomtom-international/web-sdk-maps/dist/maps.css';
+import * as turf from '@turf/turf';
 import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+
 const App = () => {
-  return <HomePage />;
+  const api_key = '8heklGGF5vF1b9RMUZZ2Rg2rlHTPB2ms';
+  const admin_key = 'WWppeLthzPxP5UjKbXipqxKKoQ4O348tS2NDdp6QkgM7s4we';
+  const project_id = '3a603903-ca43-462f-b4cc-f78e7389f5eb';
+  const [userLocation, setUserLocation] = useState({lat: 0, lon: 0});
+  const [fence_id, set_fence_id] = useState('');
+  const [geofenceData] = useState({
+    object: 'my_driver',
+    fenceName: 'my_fence',
+    time: new Date().toISOString(),
+  });
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setUserLocation({lat: latitude + 10, lon: longitude + 10});
+        },
+        error => {
+          console.error(error.message);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported');
+    }
+  }, []);
+  const mapContainer = useRef();
+  async function createGeofence(lng, lat) {
+    const request = await axios({
+      method: 'post',
+      url: `https://api.tomtom.com/geofencing/1/projects/${project_id}/fence?adminKey=${admin_key}&key=${api_key}`,
+      headers: {'Content-Type': 'application/json'},
+      data: JSON.stringify({
+        name: `Area ${Math.floor(Math.random() * 10000)}`,
+        type: 'Feature',
+        geometry: {
+          radius: 200,
+          type: 'Point',
+          shapeType: 'Circle',
+          coordinates: [lng, lat],
+        },
+      }),
+    });
+    const response = await request.data;
+    console.log({response});
+    return response.id;
+  }
+  useEffect(() => {
+    const AMSTERDAM = {
+      lon: 4.896029,
+      lat: 52.371807,
+    };
+    let map = tt.map({
+      key: api_key,
+      container: mapContainer.current.id,
+      center: AMSTERDAM,
+      zoom: 10,
+      language: 'en-GB',
+    });
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());
+    map.on('click', e => {
+      const {lng, lat} = e.lngLat;
+      const sourceID = `circleData ${Math.floor(Math.random() * 10000)}`;
+      let center = turf.point([lng, lat]);
+      let radius = 10;
+      let options = {
+        steps: 15,
+        units: 'kilometers',
+      };
+      let circle = turf.circle(center, radius, options);
+      map.addSource(sourceID, {
+        type: 'geojson',
+        data: circle,
+      });
+
+      Promise.all([createGeofence(lng, lat)]).then(result => {
+        axios({
+          method: 'get',
+          url: `https://api.tomtom.com/geofencing/1/fences/${result[0]}?key=${api_key}`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(response => response.data)
+
+          .then(result => {
+            map.addLayer({
+              id: `circle ${Math.floor(Math.random() * 10000)}`,
+              type: 'fill',
+              source: sourceID,
+              paint: {
+                'fill-color': 'blue',
+                'fill-opacity': 0.6,
+              },
+            });
+            console.log({result});
+            set_fence_id(result.id);
+          });
+      });
+    });
+
+    function addMarker(lnglat) {
+      const marker = new tt.Marker({
+        draggable: true,
+      })
+        .setLngLat(lnglat)
+        .addTo(map);
+      function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        new tt.Popup({
+          closeOnClick: false,
+        })
+
+          .setLngLat(lngLat)
+          .setHTML(
+            `<div><p> The object "${geofenceData.object}" crossed "${geofenceData.fenceName}" at ${geofenceData.time}</p></div>`
+          )
+          .addTo(map);
+        marker.on('dragend', onDragEnd);
+      }
+    }
+
+    map.on('click', e => {
+      addMarker(e.lngLat);
+    });
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div className='container'>
+      <div ref={mapContainer} className='map' id='map' />
+    </div>
+  );
 };
 
 export default App;
